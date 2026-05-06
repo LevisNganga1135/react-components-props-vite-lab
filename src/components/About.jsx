1da33e7615998e46e23d947b32d0e384a108fcae
TearsOfGod <leviskariuki2@gmail.com>
2026-05-06 17:06:07 +0300

    Updated React blog with Header props and image

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+function About ({image, about}) {
+    return(
+        <div>
+           <img src={image} alt="blog logo"/>
+           <p>{about}</p>
+        </div>
+    );
+
+
+}
+
+export default About;
